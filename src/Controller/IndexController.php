29196804db8af1ce06be414d608cc91740f6672c
fatma kerkeni <fatmakerkeni2004@gmail.com>
2025-02-23 18:23:55 +0100

    Initial commit: Symfony 7 project with Docker

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+public function home(): Response
+{
+    $articles = [
+        ['title' => 'Article 1', 'content' => 'Content 1'],
+        ['title' => 'Article 2', 'content' => 'Content 2'],
+    ];
+
+    return $this->render('articles/index.html.twig', [
+        'articles' => $articles,
+    ]);
+}
+}
